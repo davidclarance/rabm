dd2802f1fc772f42d44cef854f808c39b46e577f
Raphaël Nussbaumer <rafnuss@gmail.com>
2020-01-28 15:17:46 +0300

    Seperate extract from pull

--- R/extract_all.R
+++ R/extract_all.R
@@ -3,9 +3,8 @@
 #' @param start_date A character representing the start date from which you want to query the database. It must be in the YYYY-MM-DD format. Defaults to 1 Jan,2019. Example: '2019-01-26'.
 #' @param end_date A character representing the end date to which you want to query the database. It must be in the YYYY-MM-DD format. Defaults to 1 Feb, 2019. Example: '2019-02-26'.
 #' @param region_type Can take one of the following types: country, pentad, group, qdgc. Default is 'country'
-#' @param region_id Has to be co-defined with `region_type`. That is, it is an instantiation of the `region_type`. For instance if the `region_type` = 'country', then the `region_id` = 'kenya'. Or if `region_type` = 'pentad', then  `region_id` = '3355_1825'. Default is 'kenya'.
+#' @param region_ids A character or a vector of characters that represent the id or ids of the locations. Instantiation of (i.e. co-defined with) `region_type`. If the `region_type` = 'country', then the `region_id` must be one of the following: 'kenya', 'southafrica', 'nigeria', 'botswana', 'namibia', 'zimbabwe', 'lesotho', 'swaziland', 'mozambique'. If `region_type` = 'province', then ... . If `region_type` = 'pentad', then  `region_id` is the pentad code (e.g. '3355_1825'). If `region_type` = 'group', then  `region_id` is the group code (e.g. 'BBD'). If `region_type` = 'qdgc', then  `region_id` is the qdgccode (e.g. '???'). Default is 'kenya'.
 #' @param return_type There are two options, 'data' and 'count'. 'data' is the actual records submitted to the africa bird map. 'count' return a count of the rows. Default is 'data'.
-#' @param return_format Default s 'CSV', though the API default is 'JSON'
 #'
 #' @return Depending on the return_format, the function will return a dataframe with the following:
 #' 1. `return_type = data` A tidy dataframe is returned with the records that fall within the specification defined.
@@ -21,9 +20,8 @@
 #' start_date = '2019-01-01',
 #' end_date = '2019-02-01',
 #' region_type = 'country',
-#' region_id = 'kenya',
-#' return_type = 'data',
-#' return_format = 'CSV')
+#' region_ids = c('kenya','mozambique'),
+#' return_type = 'data')
 #'
 #'
 #'
@@ -31,57 +29,40 @@
 extract_all <- function(start_date = '2019-01-01',
                         end_date = '2019-02-01',
                         region_type = 'country',
-                        region_id = 'kenya',
-                        return_type = 'data',
-                        return_format = 'CSV') {
-  # requirements on inputs
+                        region_ids = 'kenya',
+                        return_type = 'data') {
 
+
+  # Check arguments
+  Check <- ArgumentCheck::newArgCheck()
   if (is.na(as.Date(start_date, format = "%Y-%m-%d"))) {
-    stop(message = "start_date is incorrectly specified. Please use the YYYY-MM-DD format")
-
+    ArgumentCheck::addError(msg = "'start_date' is incorrectly specified. Please use the YYYY-MM-DD format",
+                            argcheck = Check)
   }
-
-
   if (is.na(as.Date(end_date, format = "%Y-%m-%d"))) {
-    stop(message = "end_date is incorrectly specified. Please use the YYYY-MM-DD format")
-
+    ArgumentCheck::addError(msg = "'end_date' is incorrectly specified. Please use the YYYY-MM-DD format",
+                            argcheck = Check)
   }
-
-
-  region_type = tolower(region_type)
-  region_id = tolower(region_id)
-  return_type = tolower(return_type)
-
-
-  # get number of records to be returned
-
-  return_data_count <- readr::read_csv(
-    glue::glue(
-      "http://api.adu.org.za/sabap2/v2/R/{start_date}/{end_date}/{region_type}/{region_id}/count?format={return_format}"
-    ),
-    col_types = readr::cols()
-  )
-
-  if (return_data_count$records[1] > 250000) {
-    stop(message = "Your query returns more than 250,000 records. The API does not allow more than 250,000 records to be drawn at once. Please split your query.")
-
+  if (!is.character(region_type) ||
+      !region_type %in% c('country', 'province', 'pentad', 'group', 'qdgc')) {
+    ArgumentCheck::addError(msg = "'region_type' is incorrectly specified. Please use one of the following: 'country', 'province', 'pentad', 'group', 'qdgc'",
+                            argcheck = Check)
   }
+  if (!is.character(return_type) ||
+      !return_type %in% c('data', 'count')) {
+    ArgumentCheck::addError(msg = "'end_date' is incorrectly specified. Please use 'data' or 'count'",
+                            argcheck = Check)
+  }
+  ArgumentCheck::finishArgCheck(Check)
 
 
-
-  # get data
-
-
-  return_data <- readr::read_csv(
-    glue::glue(
-      "http://api.adu.org.za/sabap2/v2/R/{start_date}/{end_date}/{region_type}/{region_id}/{return_type}/all?format={return_format}"
-    ),
-    col_types = readr::cols()
-  )
-
-
-
-  return_data
+  purrr::map_df(region_ids, function(region_id) {
+    pull_single_all_location(start_date,
+                                end_date,
+                                region_type,
+                                region_id,
+                                return_type)
+  })
 
 }
 
---
+++ R/pull_single_observer_location.R
@@ -0,0 +1,84 @@
+#' Extract data for one or multiple species
+#'
+#' @param observer_ids A character that represent the ADU number that you want to query the database for. Example: observer_id = '10723'
+#' @param start_date A character representing the start date from which you want to query the database. It must be in the YYYY-MM-DD format. Defaults to 1 Jan,2019. Example: '2019-01-26'.
+#' @param end_date A character representing the end date to which you want to query the database. It must be in the YYYY-MM-DD format. Defaults to 1 Feb, 2019. Example: '2019-02-26'.
+#' @param region_type Can take one of the following types: country, province, pentad, group, qdgc. Default is 'country'
+#' @param region_id A character that represent the id of the single location. Instantiation of (i.e. co-defined with) `region_type`. If the `region_type` = 'country', then the `region_id` must be one of the following: 'kenya', 'southafrica', 'nigeria', 'botswana', 'namibia', 'zimbabwe', 'lesotho', 'swaziland', 'mozambique'. If `region_type` = 'province', then ... . If `region_type` = 'pentad', then  `region_id` is the pentad code (e.g. '3355_1825'). If `region_type` = 'group', then  `region_id` is the group code (e.g. 'BBD'). If `region_type` = 'qdgc', then  `region_id` is the qdgccode (e.g. '???'). Default is 'kenya'.
+#' @param return_type There are two options, 'data' and 'count'. 'data' is the actual records submitted to the africa bird map. 'count' return a count of the rows. Default is 'data'
+#'
+#' @export
+#'
+#' @return Depending on the return_format, the function will return a dataframe with the following:
+#' 1. `return_type = data` A tidy dataframe is returned with the records that fall within the specification defined.
+#' 2. `return_type = count` A tidy dataframe is returns with the number of rows that will be produced if the return_type was to be specified as 'data'.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' # Extract data for a single specie (variable sunbird) and location (Kenya)
+#'
+#' pull_single_observer_location(
+#' species_ids = '762',
+#' start_date = '2019-01-01',
+#' end_date = '2019-02-01',
+#' region_type = 'country',
+#' region_id = 'kenya',
+#' return_type = 'data',
+#' )
+#'
+#' }
+#'
+#'
+pull_single_observer_location <- function(observer_id,
+                                        start_date,
+                                        end_date,
+                                        region_type,
+                                        region_id,
+                                        return_type) {
+  # Check Arguments of function
+  Check <- ArgumentCheck::newArgCheck()
+  if (length(observer_id) > 1 || is.na(as.numeric(observer_id))) {
+    ArgumentCheck::addError(
+      msg = "'observer_id' is incorrectly specified. Please use a single numeric or character of numeric",
+      argcheck = Check
+    )
+  }
+  if (length(region_id) > 1) {
+    ArgumentCheck::addError(
+      msg = "'region_id' is incorrectly specified. Please use a single character",
+      argcheck = Check
+    )
+  }
+  ArgumentCheck::finishArgCheck(Check)
+
+
+  # Convert to lower case
+  region_type = tolower(region_type)
+  region_id = tolower(region_id)
+  return_type = tolower(return_type)
+
+
+  # get number of records to be returned
+  return_data_count <- readr::read_csv(
+    glue::glue(
+      "http://api.adu.org.za/sabap2/v2/R/{start_date}/{end_date}/{region_type}/{region_id}/count/observers/{species_id}?format=csv"
+    ),
+    col_types = readr::cols()
+  )$records
+
+  if (return_data_count > 250000) {
+    stop(message = "Your query returns more than 250,000 records. The API does not allow more than 250,000 records to be drawn at once. Please split your query.")
+  }
+
+  if (return_data_count > 0) {
+    print(glue::glue("Pulling data for {species_id} and for location {region_id}"))
+    readr::read_csv(
+      glue::glue(
+        "http://api.adu.org.za/sabap2/v2/R/{start_date}/{end_date}/{region_type}/{region_id}/{return_type}/observers/{species_id}?format=csv"
+      ),
+      col_types = readr::cols()
+    ) %>% add_column(region_id = region_id)
+  }
+}
