549f53ec4d284b277ab689b8ffecbffcd2ca7f99
Raphaël Nussbaumer <rafnuss@gmail.com>
2020-02-04 16:54:17 +0300

    add geojson and region name capability

--- R/find_pentad_within.R
+++ R/find_pentad_within.R
@@ -1,6 +1,10 @@
-#' Find the pentad overlapping a given shape
+#' Find the pentads covering a given shape. The shape can be given as coordinate, the name of a region or a geojson file.
 #'
+#' @param type A string 'coordinates', 'regions' or 'draw'
 #' @param coordinates A dataframe of coordinates of a shape. See `sp::Polygon` for valid coordinate structure
+#' @param regions
+#' @param geojson
+#' @param plotting
 #'
 #' @return list of pentad code string
 #'
@@ -14,38 +18,69 @@
 #' pentads <- find_pentad_within(coordinates)
 #'
 #'
-find_pentad_within <- function(coordinates,
+find_pentad_within <- function(type,
+                               coordinates,
+                               regions,
+                               file,
                                plotting=FALSE) {
 
-  Check <- ArgumentCheck::newArgCheck()
-  if (typeof(coordinates) != 'list') {
-    ArgumentCheck::addError(msg = "`coordinates` needs to be a dataframe/list",
-                            argcheck = Check)
+Check <- ArgumentCheck::newArgCheck()
+  # if (!is.character(type) ||
+  #     !type %in% c('coordinates', 'region', 'draw')) {
+  #   ArgumentCheck::addError(msg = "'type' is incorrectly specified. Please use one of the following: 'coordinates', 'region', 'draw'",
+  #                           argcheck = Check)
   }
-  if (!("lat" %in% colnames(coordinates))) {
-    ArgumentCheck::addError(msg = "`coordinates` needs to have a column `lat`",
-                            argcheck = Check)
-  }
-  if (!("lon" %in% colnames(coordinates))) {
-    ArgumentCheck::addError(msg = "`coordinates` needs to have a column `lon`",
-                            argcheck = Check)
-  }
+  # if (typeof(coordinates) != 'list') {
+  #   ArgumentCheck::addError(msg = "`coordinates` needs to be a dataframe/list",
+  #                           argcheck = Check)
+  # }
+  # if (!("lat" %in% colnames(coordinates))) {
+  #   ArgumentCheck::addError(msg = "`coordinates` needs to have a column `lat`",
+  #                           argcheck = Check)
+  # }
+  # if (!("lon" %in% colnames(coordinates))) {
+  #   ArgumentCheck::addError(msg = "`coordinates` needs to have a column `lon`",
+  #                           argcheck = Check)
+  # }
   if ( !is.logical(plotting) ) {
     ArgumentCheck::addError(msg = "`plotting` needs to be `TRUE` or `FALSE`",
                             argcheck = Check)
   }
   ArgumentCheck::finishArgCheck(Check)
 
+  if (type=='coordinates') {
+    # Create the SpatialPolygons object of coordinates queries
+    poly_query <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coordinates)), 1)))
+
+  } else if (type == 'file') {
+    #poly_query <- geojsonio::geojson_read(file)
+    poly_query <- rgdal::readOGR(file)
+    if (class(poly_query)[1] != 'SpatialPolygonsDataFrame'){
+      stop('The geojson file does not contain a polygon shape')
+    }
+    coordinates <- data.frame( t(poly_query@bbox))
+    colnames(coordinates) <- c('lat','lon')
+    sp::proj4string(poly_query) <- NA_character_
+
+  } else if (type == 'region') {
+    region_query <- maps::map(regions = regions, plot = FALSE, fill=TRUE)
+    poly_query <- maptools::map2SpatialPolygons(region_query, IDs=region_query$names)
+    coordinates <- data.frame(lat = region_query$x, lon = region_query$y)
+  }
+
+
+
   # Rounding to the grid resolution (0.2°)
   format_pentad <- function(x) {
     sign(x)*floor(abs(x)/0.2)*0.2
   }
 
+
   # Generate a list of all possible pentads covering the extremum of the coordinates queries
   pentads_possible<-list()
   i <- 1
-  for (i_lat in seq( format_pentad(min(coordinates$lat)), format_pentad(max(coordinates$lat)), 0.2)){
-    for (i_lon in seq( format_pentad(min(coordinates$lon)), format_pentad(max(coordinates$lon)), 0.2)){
+  for (i_lat in seq( format_pentad(min(coordinates$lat, na.rm = TRUE)), format_pentad(max(coordinates$lat, na.rm = TRUE)), 0.2)){
+    for (i_lon in seq( format_pentad(min(coordinates$lon, na.rm = TRUE)), format_pentad(max(coordinates$lon, na.rm = TRUE)), 0.2)){
       pentads_possible[[i]] <- sp::Polygons(list(
         sp::Polygon( cbind(
           c(i_lat, i_lat+0.2, i_lat+0.2, i_lat, i_lat),
@@ -59,11 +94,9 @@ find_pentad_within <- function(coordinates,
   # Create the SpatialPolygons object of the possible pentads
   pentads_possible_poly <- sp::SpatialPolygons(pentads_possible)
 
-  # Create the SpatialPolygons object of coordinates queries
-  coordinates_poly <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coordinates)), 1)))
 
   # Check which possible pentads overlap with the coordinate
-  pentads_id <- !is.na(sp::over(pentads_possible_poly, coordinates_poly))
+  pentads_id <- !is.na(sp::over(pentads_possible_poly, poly_query))
 
   # Select only the pentads which are inside
   pentads_inside <- pentads_possible_poly[pentads_id]
@@ -76,12 +109,14 @@ find_pentad_within <- function(coordinates,
   p@data$pentad_name <- find_pentad(data.frame(lat=pentads_coord[,1],lon=pentads_coord[,2]))
 
 
+
   if (plotting){
     leaflet::leaflet() %>%
       leaflet::addTiles() %>%
+      leaflet.extras::addDrawToolbar() %>%
       leaflet::addPolygons(data=p,
                            label = ~paste0(pentad_name)) %>%
-      leaflet::addPolygons(data=coordinates_poly, fill = FALSE, color = 'red')
+      leaflet::addPolygons(data=poly_query, fill = FALSE, color = 'red')
   }
 
   p@data$pentad_name
